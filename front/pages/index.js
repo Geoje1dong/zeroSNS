c935fb929f26aad16f60a5fa0125b828f6e07915
Kim Jae Hyune <wogus4041@naver.com>
2019-06-12 17:17:26 +0900

    ant design 적용하기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return (
         <>
-            <div>Hello, Next!</div>
-            <Link href="/about"><a>About</a></Link>
+            <Head>
+                <title>zeroSNS</title>
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.css" />
+                <script src='https://cdnjs.cloudflare.com/ajax/libs/antd/3.19.0/antd.js'/>
+            </Head>
+            <AppLayout>
+                <div>Hello, Next!</div>
+                <Link href="/about"><a>About</a></Link>
+            </AppLayout>
         </>
     )
 }
