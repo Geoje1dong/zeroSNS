5147a213095f07260f44ee9f96c0b7eabea9e198
Kim Jae Hyune <wogus4041@naver.com>
2019-06-13 20:23:26 +0900

    프로필 화면 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import AppLayout from '../components/AppLayout';
+import {Form, Button, List, Card, Input, Icon} from 'antd';
 
 const Profile = () => {
     return(
         <React.Fragment>
-            <div>프로필</div>
+            <Form>
+                <Input addonBefore='닉네임'/>
+                <Button type='primary'>수정</Button>
+            </Form>
+            <List
+                grid={{gutter:4, xs:2, md:3}}
+                size='small'
+                header={<div>팔로워 목록</div>}
+                loadMore={<Button>더 보기</Button>}
+                bordered
+                dataSource={['제로초', '보보', '노드버드오피셜']}
+                renderItem={item => (
+                    <List.Item >
+                        <Card actions={[<Icon key='stop' type='stop' />]}>
+                            <Card.Meta description={item} />
+                        </Card>
+                    </List.Item>
+                )}
+            />
+            <List
+                grid={{gutter:4, xs:2, md:3}}
+                size='small'
+                header={<div>팔로잉 목록</div>}
+                loadMore={<Button>더 보기</Button>}
+                bordered
+                dataSource={['제로초', '보보', '노드버드오피셜']}
+                renderItem={item => (
+                    <List.Item >
+                        <Card actions={[<Icon key='stop' type='stop' />]}>
+                            <Card.Meta description={item} />
+                        </Card>
+                    </List.Item>
+                )}
+            />
         </React.Fragment>
     )
 }
